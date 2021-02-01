5f8155108f2c538dd176c638850b7ea185d79028
spenser6131 <brinkman.spenser@gmail.com>
2021-01-31 16:39:47 -0800

    rename rooms to spaces

--- app/models/room.rb
+++
@@ -1,7 +0,0 @@
-class Room < ApplicationRecord
-  has_many :plants
-
-  validates :name, :humidity, :light, presence: true
-  validates :name, uniqueness: true
-  validates :humidity, :light, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
-end
