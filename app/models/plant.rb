8c4c73b1c230d0ecf075ccc1c3270866f1421ddf
spenser6131 <brinkman.spenser@gmail.com>
2021-01-31 16:40:30 -0800

    plant model

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,6 +1,8 @@
 class Plant < ApplicationRecord
-  belongs_to :room
+  belongs_to :space, optional: true
 
-  validates :species, :light_needs, :humidity_needs, :water_frequency, :last_watering, :last_fertilization, presence: true
-  validates :light_needs, :humidity_needs, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  validates :species, :light_requirement, :humidity_requirement, :water_frequency, :last_watering, :last_fertilization, presence: true
+  validates :light_requirement, :humidity_requirement, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+
+  scope :unassigned, -> {where(space_id: nil)}
 end
